db30719e6f8285c6aab1625026b1e138614e3c13
100134969 <kingpatrick455@gmail.com>
2019-09-05 08:56:04 -0500

    turtlegallery

---
+++ TurtleArtGallery.py
@@ -0,0 +1,40 @@
+import turtle
+turtle.color("red")
+turtle.speed(2)
+
+
+def square(sidelength):
+    for i in range(9):
+       turtle.forward(sidelength)
+       turtle.right(72)
+square(90)
+turtle.penup()
+turtle.forward(155)
+turtle.pendown()
+def square(sidelength):
+    for i in range(9):
+       turtle.forward(sidelength)
+       turtle.right(72)
+square(90)
+turtle.penup()
+turtle.forward(155)
+turtle.pendown()
+
+def square(sidelength):
+    for i in range(9):
+       turtle.forward(sidelength)
+       turtle.right(72)
+square(90)
+turtle.penup()
+turtle.forward(155)
+turtle.pendown()
+def square(sidelength):
+    for i in range(9):
+       turtle.forward(sidelength)
+       turtle.right(72)
+square(90)
+turtle.penup()
+turtle.forward(155)
+turtle.pendown()
+turtle.exitonclick()
+
