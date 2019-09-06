80af9e42101116ee3fc98c9c7ff15208c371f043
100134969 <kingpatrick455@gmail.com>
2019-09-06 08:25:41 -0500

    turtlegallery

--- TurtleArtGallery.py
+++ TurtleArtGallery.py
@@ -1,8 +1,7 @@
 import turtle
 turtle.color("red")
 turtle.speed(2)
-
-
+turtle.bgcolor("black")
 def square(sidelength):
     for i in range(9):
        turtle.forward(sidelength)
@@ -15,11 +14,20 @@ def square(sidelength):
     for i in range(9):
        turtle.forward(sidelength)
        turtle.right(72)
+
 square(90)
 turtle.penup()
 turtle.forward(155)
 turtle.pendown()
 
+def square(sidelength):
+    for i in range(9):
+       turtle.forward(sidelength)
+       turtle.right(72)
+square(90)
+turtle.penup()
+turtle.forward(155)
+turtle.pendown()
 def square(sidelength):
     for i in range(9):
        turtle.forward(sidelength)
